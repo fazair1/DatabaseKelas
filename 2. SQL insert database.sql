b8e986920f17bf9f3e31a7cea307904115dcae16
OwenJTZ <97722239+OwenJTZ@users.noreply.github.com>
2022-01-14 20:19:29 +0700

    Update 2. SQL insert database.sql

---
+++ 2. SQL insert database.sql
@@ -0,0 +1,42 @@
+INSERT INTO Pembeli VALUES 
+('PU001', '', '', ''),
+('PU002', '', '', ''),
+('PU003', '', '', ''),
+('PU004', '', '', ''),
+('PU005', '', '', '')
+
+INSERT INTO Rak VALUES 
+('RA001', 'RakFilmCampuran'),
+('RA002', 'RakFilmDrama'),
+('RA003', 'RakFilmAnimasi'),
+('RA004', 'RakFilmKomedi'),
+('RA005', 'RakFilmHorror'),
+('RA006', 'RakFilmRomance'),
+('RA007', 'RakFilmAction'),
+('RA008', 'RakFilmFantasi'),
+('RA009', 'RakFilmPetualangan'),
+('RA010', 'RakFilmSciFi'),
+
+
+INSERT INTO DVD VALUES 
+('DVD001', '', '', '', '','',''),
+('DVD002', '', '', '', '','',''),
+('DVD003', '', '', '', '','',''),
+('DVD004', '', '', '', '','',''),
+('DVD005', '', '', '', '','','')
+
+INSERT INTO Orderr VALUES 
+('OI001', '', '2021-12-21', '2'),
+('OI002', '', '2021-08-13', '6'),
+('OI003', '', '2021-05-28', '1'),
+('OI004', '', '2022-01-13', '3'),
+('OI005', '', '2022-01-09', '3')
+
+INSERT INTO Pembayaran VALUES 
+('PY001', '', '', ''),
+('PY001', '', '', ''),
+('PY001', '', '', ''),
+('PY001', '', '', ''),
+('PY001', '', '', '')
+
+
