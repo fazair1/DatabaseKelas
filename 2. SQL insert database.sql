3668b08fee5c53085ef5f84bb2586cec2acccd08
fazair1 <91309987+fazair1@users.noreply.github.com>
2022-01-15 14:45:40 +0700

    Update 2. SQL insert database.sql

--- 2. SQL insert database.sql
+++ 2. SQL insert database.sql
@@ -19,11 +19,21 @@ INSERT INTO Rak VALUES
 
 
 INSERT INTO DVD VALUES 
-('DVD001', '', '', '', '','',''),
-('DVD002', '', '', '', '','',''),
-('DVD003', '', '', '', '','',''),
-('DVD004', '', '', '', '','',''),
-('DVD005', '', '', '', '','','')
+('DVD001', 'RA001', 'Sarung Wars', 'Action', 'Budi','A-2',10000),
+('DVD002', 'RA001', 'Dimas si Kereta', 'Animation', 'Anton','KarToon',15000),
+('DVD003', 'RA002', 'Terjebak di Rumah', 'Drama', 'Joko','I-Drama',11000),
+('DVD004', 'RA002', 'Menolak COD', 'Drama', 'Rafi','I-Drama',12000),
+('DVD005', 'RA003', 'Weaboo', 'Animasi', 'Tanjiro','KarToon',14000),
+('DVD006', 'RA003', 'Udin Idin', 'Animasi', 'Bagus','KarToon',12000),
+('DVD007', 'RA004', 'Tretan dan Narkoboy', 'Komedi', 'Tretan','MLI',10000),
+('DVD008', 'RA004', 'DediBotak', 'Komedi', 'Dedi','POD',15000),
+('DVD009', 'RA005', 'Kereta ke Tanah Abang', 'Horror', 'Udin','HariHari',14000),
+('DVD0010', 'RA006', 'Aku, Dia dan Kami', 'Romance', 'Reza','Cie',16000),
+('DVD0011', 'RA007', 'Ke Raid', 'Action', 'Raha','A-2',11000),
+('DVD0012', 'RA008', 'Pacar', 'Fantasi', 'Cindy','Mimpi-S',14000),
+('DVD0013', 'RA008', 'Naga Terbang', 'Fantasi', 'Dika','Indosiar',20000),
+('DVD0014', 'RA009', 'Cilang', 'Petualangan', 'Andi','CRIT',10000),
+('DVD0015', 'RA0010', 'Sesajen', 'Sci-Fi', 'Gagah','One',12000);
 
 INSERT INTO Orderr VALUES 
 ('OI001', '', '2021-12-21', '2'),
