76b51560dc35725c74f679d06bdf700e584429e6
fazair1 <fauzan.irfanto1@gmail.com>
2022-01-16 14:07:54 +0700

    Update 2. SQL insert database.sql

--- 2. SQL insert database.sql
+++ 2. SQL insert database.sql
@@ -3,7 +3,7 @@ INSERT INTO Pembeli VALUES
 ('PU002', 'William', '08195412942', 'Jakarta Utara'),
 ('PU003', 'Valentina', '085730451953', 'Pekanbaru'),
 ('PU004', 'Steven', '087650421444', 'Surabaya'),
-('PU005', 'Gabriel', '081517421122', 'Pontianak')
+('PU005', 'Gabriel', '081517421122', 'Pontianak');
 
 INSERT INTO Rak VALUES 
 ('RA001', 'RakFilmCampuran'),
@@ -15,7 +15,7 @@ INSERT INTO Rak VALUES
 ('RA007', 'RakFilmAction'),
 ('RA008', 'RakFilmFantasi'),
 ('RA009', 'RakFilmPetualangan'),
-('RA010', 'RakFilmSciFi'),
+('RA010', 'RakFilmSciFi');
 
 
 INSERT INTO DVD VALUES 
@@ -28,25 +28,25 @@ INSERT INTO DVD VALUES
 ('DVD007', 'RA004', 'Tretan dan Narkoboy', 'Komedi', 'Tretan','MLI',10000),
 ('DVD008', 'RA004', 'DediBotak', 'Komedi', 'Dedi','POD',15000),
 ('DVD009', 'RA005', 'Kereta ke Tanah Abang', 'Horror', 'Udin','HariHari',14000),
-('DVD0010', 'RA006', 'Aku, Dia dan Kami', 'Romance', 'Reza','Cie',16000),
-('DVD0011', 'RA007', 'Ke Raid', 'Action', 'Raha','A-2',11000),
-('DVD0012', 'RA008', 'Pacar', 'Fantasi', 'Cindy','Mimpi-S',14000),
-('DVD0013', 'RA008', 'Naga Terbang', 'Fantasi', 'Dika','Indosiar',20000),
-('DVD0014', 'RA009', 'Cilang', 'Petualangan', 'Andi','CRIT',10000),
-('DVD0015', 'RA0010', 'Sesajen', 'Sci-Fi', 'Gagah','One',12000);
+('DVD010', 'RA006', 'Aku, Dia dan Kami', 'Romance', 'Reza','Cie',16000),
+('DVD011', 'RA007', 'Ke Raid', 'Action', 'Raha','A-2',11000),
+('DVD012', 'RA008', 'Pacar', 'Fantasi', 'Cindy','Mimpi-S',14000),
+('DVD013', 'RA008', 'Naga Terbang', 'Fantasi', 'Dika','Indosiar',20000),
+('DVD014', 'RA009', 'Cilang', 'Petualangan', 'Andi','CRIT',10000),
+('DVD015', 'RA010', 'Sesajen', 'Sci-Fi', 'Gagah','One',12000);
 
 INSERT INTO Orderr VALUES 
-('OI001', '', '2021-12-21', '2'),
-('OI002', '', '2021-08-13', '6'),
-('OI003', '', '2021-05-28', '1'),
-('OI004', '', '2022-01-13', '3'),
-('OI005', '', '2022-01-09', '3')
+('OI001', 'DVD001', '2021-12-21', '2'),
+('OI002', 'DVD003', '2021-08-13', '6'),
+('OI003', 'DVD005', '2021-05-28', '1'),
+('OI004', 'DVD002', '2022-01-13', '3'),
+('OI005', 'DVD014', '2022-01-09', '3');
 
 INSERT INTO Pembayaran VALUES 
-('PY001', 'OI001', '2021-12-22', 10000),
-('PY001', 'OI002', '2021-08-13', 30000),
-('PY001', 'OI003', '2021-05-30', 5000),
-('PY001', 'OI004', '2022-01-13', 15000),
-('PY001', 'OI005', '2022-01-10', 15000)
+('PY001', 'OI001', 'PU001', '2021-12-22', 10000),
+('PY001', 'OI002', 'PU002', '2021-08-13', 11000),
+('PY001', 'OI003', 'PU003', '2021-05-30', 14000),
+('PY001', 'OI004', 'PU004', '2022-01-13', 15000),
+('PY001', 'OI005', 'PU005', '2022-01-10', 10000);
 
 
